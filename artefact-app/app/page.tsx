4ca63a8b5e25e4ac0a5e140df4c8e59de033b886
Lunna Rayna <48037282+lunnarayna@users.noreply.github.com>
2026-05-23 21:56:14 +0000

    bug fix: any variable

--- artefact-app/app/page.tsx
+++ artefact-app/app/page.tsx
@@ -2,9 +2,11 @@
 
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
+import { User } from "@supabase/supabase-js"; 
 
 export default function Home() {
-  const [user, setUser] = useState<any>(null);
+  
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const getUser = async () => {
